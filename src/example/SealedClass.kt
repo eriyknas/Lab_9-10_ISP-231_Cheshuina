f63515996cb6c07a6766fb5205da162d8cdb8620
Sliceeee01 <ivan_hih@icloud.com>
2026-01-21 14:20:39 +0300

    learn sealed-class

---
+++ src/example/SealedClass.kt
@@ -0,0 +1,31 @@
+package example
+
+import com.sun.nio.sctp.HandlerResult
+import javax.sound.midi.MetaMessage
+
+sealed class NetworkResult {
+    data class Success(val data: String) : NetworkResult()
+    data class Error(val message: String, val code: Int) : NetworkResult()
+    object Loading : NetworkResult()
+}
+fun handlerResult(result: NetworkResult) {
+    when (result) {
+        is NetworkResult.Success -> {
+            println("Успех: ${result.data}")
+        }
+        is NetworkResult.Error -> {
+            println("Ошибка ${result.code}: ${result.message}")
+        }
+        NetworkResult.Loading -> {
+            println("Загрузка...")
+        }
+    }
+}
+fun main() {
+    val success = NetworkResult.Success("Данные получены")
+    val error = NetworkResult.Error("Сервер не отвечает",500)
+    val loading = NetworkResult.Loading
+    handlerResult(success)
+    handlerResult(error)
+    handlerResult(loading)
+}
