24ce81836270f632ee9e72f7bacb3b5ec0f144e0
eriyknas <anastasia9cheshuina@yandex.ru>
2026-01-28 05:50:15 +0300

    final: delegation, lazy and file persistence

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import SystemLogger.logger
 import modules.ModuleResult
 import resources.OutpostResource
 import resources.ResourceManager
@@ -16,13 +17,32 @@ fun handleModuleResult(result: ModuleResult){
             println("ОШИБКА: ${result.reason}")
     }
 }
+object SystemLogger{
+    val logger by lazy {
+        SystemLogger
+    }
+    init {
+        println("SystemLogger инициализирован")
+    }
+    fun log(message: String){
+        println("[LOG] $message")
+    }
+}
 fun main(){
+    logger.log("Запуск базы")
     val manager = ResourceManager()
     val minerals = OutpostResource(1,"Minerals",300)
     val gas = OutpostResource(2,"Gas",100)
     manager.add(minerals)
     manager.add(gas)
     manager.printAll()
-    val bonus = minerals.copy(amount = minerals.amount +50)
+    val bonus = minerals.copy(amountInit = minerals.amount + 50)
     println("Копия минералов с бонусом: $bonus")
+    val loadedResources = FileStorage.load()
+    loadedResources.forEach { manager.add(it) }
+    if (loadedResources.isEmpty()){
+        manager.add(OutpostResource(1,"Minerals",300))
+        manager.add(OutpostResource(2,"Gas",100))
+    }
+    FileStorage.save(manager.getAll())
 }
